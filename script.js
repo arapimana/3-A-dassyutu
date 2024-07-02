64adf24031122dc7996d5c345b76b64a5af9db76
arapimana <129587248+arapimana@users.noreply.github.com>
2024-07-03 00:46:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,9 @@ function checkAnswer() {
     const answerInput = document.getElementById('answerInput').value;
     const result = document.getElementById('result');
 
-    if (answerInput === "解決") {
+    if (answerInput === "解答") {
         result.textContent = "おめでとう！";
     } else if (answerInput === "真相") {
-        result.textContent = "完全解決";
+        result.textContent = "完全解決　ありがとうございました！";
     } else {
-        result.textContent = "残念";
-    }
-}
+        result.textContent = "誤答
